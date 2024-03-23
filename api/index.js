929e7eea7d698012a3b2f186efa119df842896c4
Rohan Pandya <rohanpandya07@gmail.com>
2024-03-23 16:35:41 +0530

    minor changes

--- api/index.js
+++ api/index.js
@@ -7,37 +7,37 @@ import { dirname } from 'path';
 import imageDownloader from "image-downloader";
 import Job from "./models/Job.js";
 dotenv.config();
-const app=express()
+const app = express()
 app.use(express.json());
 
 app.use(cors({
-    credentials:true,
-    origin:'http://localhost:3000',
+    credentials: true,
+    origin: 'http://localhost:3000',
 }))
 const __dirname = fileURLToPath(dirname(import.meta.url));
-app.use('/uploads', express.static(__dirname+'/uploads'));
+app.use('/uploads', express.static(__dirname + '/uploads'));
 // console.log(__dirname);
 mongoose.connect(process.env.MONGO_URL)
-app.get('/test',(req,res)=>{
+app.get('/test', (req, res) => {
     const searchText = req.query.searchText;
     res.json(searchText)
 })
 
-app.post('/upload-by-link',async(req,res) =>{
-    const {link} = req.body;
-    
-    const newName= 'photo'+ Date.now()+'.jpg';
+app.post('/upload-by-link', async (req, res) => {
+    const { link } = req.body;
+
+    const newName = 'photo' + Date.now() + '.jpg';
     await imageDownloader.image({
-        url:link,
-        dest:__dirname+newName,
+        url: link,
+        dest: __dirname + newName,
     })
     res.json(newName);
 
 })
-app.post('/jobs',async(req,res)=>{
-    const {jobtitle,joblocation,jobcompany,perks,salary,description}=req.body;
-    try{
-        const JobDoc=await Job.create({
+app.post('/jobs', async (req, res) => {
+    const { jobtitle, joblocation, jobcompany, perks, salary, description } = req.body;
+    try {
+        const JobDoc = await Job.create({
             jobtitle,
             joblocation,
             jobcompany,
@@ -46,14 +46,14 @@ app.post('/jobs',async(req,res)=>{
             description
         })
         res.json(JobDoc);
-    }catch(err){
+    } catch (err) {
         res.status(422).json(err)
     }
 })
-app.put('/jobs',async(req,res)=>{
-    const {id,jobtitle,joblocation,jobcompany,perks,salary,description}=req.body;
-    try{
-        const JobDoc=await Job.findById(id)
+app.put('/jobs', async (req, res) => {
+    const { id, jobtitle, joblocation, jobcompany, perks, salary, description } = req.body;
+    try {
+        const JobDoc = await Job.findById(id)
         JobDoc.set({
             jobtitle,
             joblocation,
@@ -64,15 +64,25 @@ app.put('/jobs',async(req,res)=>{
         })
         await JobDoc.save();
         res.json('ok');
-    }catch(err){
+    } catch (err) {
         res.status(422).json(err)
     }
 })
-app.get('/jobs/:id',async (req,res)=>{
-    const {id}=req.params;
+app.delete('/jobs/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        await Job.deleteOne({ _id: id });
+        res.json({ message: 'job deleted successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to delete job' });
+    }
+})
+app.get('/jobs/:id', async (req, res) => {
+    const { id } = req.params;
     res.json(await Job.findById(id));
 })
-app.get('/jobs',async (req,res)=>{
-    res.json( await Job.find() )
+app.get('/jobs', async (req, res) => {
+    res.json(await Job.find())
 })
 app.listen(4000);
